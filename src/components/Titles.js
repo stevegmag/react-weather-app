1abe641eb843c73679d65ed51666a1015718c81d
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-04-06 17:47:25 -0400

    updated scss and added some style

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ class Titles extends Component {
   render() {
     return (
       <div className="title">
-        <h1>Weather Finder</h1>
+        <h1>US Weather Finder</h1>
         <p>Find out temperature, conditions and more...</p>
       </div>
     );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,25 +4,30 @@ class Weather extends Component {
   render() {
     return (
       <div className="weather">
-        <h2>Weather components here</h2>
+        <h2>
+        Current Conditions for { this.props.city }, { this.props.state } <br />
+        <span>Date / Time: { this.props.localtime }</span>
+        { /* swap state field with pulldown generated from remote json */ }
+        </h2>
 
-        { this.props.city } <br />
-        { this.props.state } <br />
-        { this.props.localtime } <br />
+        { this.props.condition_icon && <img src={ this.props.condition_icon } />} <br />
+        { this.props.condition_text } <br />
+        
+        Temp: { this.props.temp_f } °<br />
+        Feels Like: { this.props.feelslike_f } °<br />
+        Precipitation (inches): { this.props.precip_in }" <br />
+        Wind: { this.props.wind_mph } mph<br />
+        Wind Direction: { this.props.wind_dir } <br />
+        Vidibility: { this.props.vis_miles } mi<br />
+        { this.props.error }
+
+        <div classname="hide-me">
+        
+        { this.props.humidity } <br />
         { this.props.lat } <br />
         { this.props.lon } <br />
         { this.props.tz_id } <br />
-        { this.props.temperature } <br />
-        { this.props.humidity } <br />
-        { this.props.condition_text } <br />
-        { this.props.condition_icon} <br />
-        { this.props.feelslike_f } <br />
-        { this.props.temp_f } <br />
-        { this.props.precip_in } <br />
-        { this.props.wind_dir } <br />
-        { this.props.wind_mph } <br />
-        { this.props.vis_miles } <br />
-        { this.props.error }
+        </div>
 
       </div>
     );
